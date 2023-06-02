5a1cc40edcdaf49b1ed3f8f255612c80c4676f90
nick.kushnir <nick.kushnir@liveperson.com>
2023-06-02 02:38:35 +0200

    fixes: add routing

---
+++ chocolate-openai/src/components/about-us/AboutUs.js
@@ -0,0 +1,28 @@
+function AboutUs() {
+
+    return (
+        <>
+            <div className='w-[720px] mx-auto py-24'>
+                <div className='w-full items-center px-8'>
+                    <p
+                        className='block text-gray-500 font-bold text-center mb-1 md:mb-0 pr-4'>
+                        About Us
+                    </p>
+                </div>
+                <div className='flex justify-between'>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting, remaining essentially unchanged. It was
+                        popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                        and more recently with desktop publishing software like Aldus PageMaker including versions of
+                        Lorem Ipsum.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AboutUs;
